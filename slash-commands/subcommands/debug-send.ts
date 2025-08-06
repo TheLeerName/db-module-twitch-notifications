a83141a0969457524233091a2964711622908708
Leer <85291330+TheLeerName@users.noreply.github.com>
2025-08-07 03:48:30 +0700

    fix of twitch debug-send slash command

--- slash-commands/subcommands/debug-send.ts
+++ slash-commands/subcommands/debug-send.ts
@@ -34,7 +34,9 @@ const command = new SlashSubcommand()
 
 		const response = await Request.OAuth2Validate(authorization);
 		if (!response.ok) await refreshToken();
+
 		var status = "Отключён";
+		var s_check = "*неизвестно*";
 		if (connection) {
 			if (connection.ws.readyState === connection.ws.CONNECTING) {
 				status = "Подключение";
@@ -44,35 +46,42 @@ const command = new SlashSubcommand()
 				status = `Открыт, подключён через \`${connection.ws.url}\` уже ${humanizeDuration(t - connection.first_connected_timestamp)} (текущая сессия ${humanizeDuration(t - connection.getConnectedTimestamp())})`;
 
 				const subscriptions: Record<string, ResponseBody.GetEventSubSubscriptions["data"][0]> = {};
+				var atleast_one: boolean = false;
 				var cursor: string | undefined;
 				while(true) {
 					const response = await Request.GetEventSubSubscriptions(authorization, undefined, undefined, undefined, undefined, cursor);
 					if (response.ok) {
 						if (response.data.length > 0) {
+							atleast_one = true;
 							response.data.forEach(s => subscriptions[s.id] = s);
 							if (response.pagination?.cursor) {
 								cursor = response.pagination.cursor;
 								await new Promise<void>(resolve => setTimeout(resolve, 500));
 								continue;
 							}
+							else
+								break;
 						}
+						else
+							break;
 					}
 					else {
-						cursor = undefined;
-						status += `, проверка подписок завершилась с кодом ${response.status} - ${response.message}`;
+						s_check = `Ошибка! ${response.status} - ${response.message}`;
 						break;
 					}
 				}
-				if (cursor) {
-					status += `, проверка подписок завершилась успешно: `;
+				if (atleast_one) {
+					s_check = "";
 					Object.values(data.global.channels).forEach(c => {
-						status += `${c.user.display_name} => `;
+						if (c.subscriptions_id.length < 1) return; 
+						s_check += `${c.user.display_name} => `;
 						c.subscriptions_id.forEach(id => {
-							status += `\`${id}\` = \`"${subscriptions[id]?.status ?? "not_found_by_api"}"\`, `;
+							const s = subscriptions[id];
+							s_check += `\`${s?.type ? `${s.type}:` : ""}${id}\` = \`"${s?.status ?? "not_found_by_api"}"\`, `;
 						});
-						status = `${status.substring(0, status.length - 2)}; `;
+						s_check = `${s_check.substring(0, s_check.length - 2)}; `;
 					});
-					status = status.substring(0, status.length - 2);
+					if (s_check.length > 0) s_check = s_check.substring(0, s_check.length - 2);
 				}
 			}
 			if (connection.ws.readyState === connection.ws.CLOSING) {
@@ -96,6 +105,10 @@ const command = new SlashSubcommand()
 				{
 					name: "Состояние EventSub",
 					value: status
+				},
+				{
+					name: "Проверка подписок EventSub",
+					value: s_check
 				},
 				{
 					name: "Сохранённых каналов",
