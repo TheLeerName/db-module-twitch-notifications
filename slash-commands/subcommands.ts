0ca46cf105bebddc8a1abe45ea03ded742389795
Leer <85291330+TheLeerName@users.noreply.github.com>
2025-12-18 07:52:05 +0700

    change twitch debug command
    and remove twitch debug-send (twitch debug already does this)

--- slash-commands/subcommands.ts
+++ slash-commands/subcommands.ts
@@ -4,7 +4,6 @@ import { module_name } from './../index';
 import channelAdd from './subcommands/channel-add';
 import channelRemove from './subcommands/channel-remove';
 import channelSend from './subcommands/channel-send';
-import debugSend from './subcommands/debug-send';
 import debug from './subcommands/debug';
 import send from './subcommands/send';
 import setCategory from './subcommands/set-category';
@@ -16,7 +15,7 @@ const command = new SlashCommand()
 .setName('twitch')
 .setDescription(`Contains all commands of "${module_name}" module`)
 .setDescriptionLocalization('ru', `Содержит все команды модуля "${module_name}"`)
-.addSubcommands(channelAdd, channelRemove, channelSend, debugSend, debug, send, setCategory, setRole);
+.addSubcommands(channelAdd, channelRemove, channelSend, debug, send, setCategory, setRole);
 
 export function main(): SlashCommandBuilder[] {
 	return [command];
--- slash-commands/subcommands/debug.ts
+++ slash-commands/subcommands/debug.ts
@@ -1,8 +1,6 @@
 import { SlashSubcommand, humanizeDuration } from '../../../../core/slash-commands';
 import { config } from '../../../../core';
-import { data, config_section } from '../../index';
 import * as L from '../../../../core/logger';
-import * as Messages from '../../messages';
 import * as Main from '../../index';
 
 import { EmbedBuilder, Message } from 'discord.js';
@@ -12,19 +10,6 @@ export function setBotCreatorDiscordID(v: string) {
 	botCreatorDiscordID = v;
 }
 
-const user = {
-	login: "smart",
-	display_name: "Умный человек в очках",
-	description: "bla bla bla",
-	profile_image_url: "https://avatars.mds.yandex.net/get-kinopoisk-image/1600647/b93c8f78-f7cc-4f31-ab92-6d293d355371/360",
-	offline_image_url: "https://henley-festival.co.uk/wp-content/uploads/Rick-Astley2-S.png",
-	broadcaster_type: ""
-} as const;
-var title = "Название";
-var games = ["Игра 1", "Игра 2", "Игра 3"];
-var viewer_count = 69;
-var started_at: string = ""; 
-
 const command = new SlashSubcommand()
 .setName('debug')
 .setDescription(`Does some debug commands of "${Main.module_name}" module. Works for bot creator only!`)
@@ -32,9 +17,11 @@ const command = new SlashSubcommand()
 .setChatInput(async(interaction) => {
 	if (interaction.guild == null) return;
 
-	const start = Date.now();
-	await interaction.deferReply();
 	const type = interaction.options.getString("type")!;
+	const isEphemeral = type === "gettoken";
+
+	const start = Date.now();
+	await interaction.deferReply({ flags: isEphemeral ? "Ephemeral" : undefined });
 	const values = (interaction.options.getString("values") ?? "").split(";");
 	try {
 		if (type.length < 1) throw new Error("Параметр type не указан");
@@ -43,128 +30,55 @@ const command = new SlashSubcommand()
 		if (botCreatorDiscordID.length > 0 && botCreatorDiscordID != interaction.user.id)
 			throw new Error("Вы не являетесь создателем бота");
 
-		if (type === "testmessage") {
-			title = "Название";
-			games = ["Игра 1", "Игра 2", "Игра 3"];
-			viewer_count = 69;
-			started_at = new Date().toISOString();
-			const msg = Messages.streamStart(user, {
-				title,
-				games,
-				started_at
-			}, {
-				viewer_count,
-				thumbnail_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPg3Z19paW_g0iFfa5k0iJjFR4HKyAVkl6A&s"
-			});
-			const reply = await interaction.editReply({ content: msg.content, embeds: msg.embeds });
-			const thread = await Main.getThread(reply);
-			await thread.send(Main.getDiscordMessagePrefix(`:speech_left: Название стрима: **${title}**`));
-			await thread.send(Main.getDiscordMessagePrefix(`:video_game: Текущая игра: **${games[games.length - 1]}**`));
-			await thread.send(Main.getDiscordMessagePrefix(`:bust_in_silhouette: Зрителей: **${viewer_count}**`));
+		if (type === "wsstatus") {
+			const status = (() => { switch(Main.connection?.ws.readyState) {
+				case WebSocket.CONNECTING: return "Подключение";
+				case WebSocket.OPEN:
+					const t = Date.now();
+					return `Открыт, подключён через \`${Main.connection.ws.url}\` уже ${humanizeDuration(t - Main.connection.first_connected_timestamp)} (текущая сессия ${humanizeDuration(t - Main.connection.getConnectedTimestamp())})`;
+				case WebSocket.CLOSING: return "Закрывается";
+				case WebSocket.CLOSED: return "Закрыт";
+				default: return "Выключен";
+			}})();
+			await interaction.editReply({embeds: [new EmbedBuilder()
+				.setTitle(`:white_check_mark: Успешно!`)
+				.setDescription(`\`\`\`\n${status}\n\`\`\``)
+				.setColor("#77b255")
+				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
+			]});
 		}
-		else if (type === "testmessagechangeviewers") {
-			const message_id = values[0]; if (!message_id || message_id.length < 1) throw new Error("ID сообщения не найдено");
-			const message = await Main.getDiscordMessageByID(interaction.channelId, message_id);
-			if (message === Main.ErrorMessages.CHANNEL_WRONG_TYPE || message === Main.ErrorMessages.CHANNEL_NOT_FOUND || message === Main.ErrorMessages.MESSAGE_NOT_FOUND) throw new Error(message);
-			const value = parseInt(values[1]); if (isNaN(value)) throw new Error("Значение не найдено или не является числом");
-			viewer_count = value;
-
-			await (await Main.getThread(message)).send(Main.getDiscordMessagePrefix(`:bust_in_silhouette: Зрителей: **${viewer_count}**`));
-			await message.edit(Messages.streamStart(user, {
-				title,
-				games,
-				started_at
-			}, {
-				viewer_count,
-				thumbnail_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPg3Z19paW_g0iFfa5k0iJjFR4HKyAVkl6A&s"
-			}));
+		else if (type === "wsrestart") {
+			await Main.changeStateEventSub();
 			await interaction.editReply({embeds: [new EmbedBuilder()
 				.setTitle(`:white_check_mark: Успешно!`)
 				.setColor("#77b255")
 				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
 			]});
 		}
-		else if (type === "testmessagechangetitle") {
-			const message_id = values[0]; if (!message_id || message_id.length < 1) throw new Error("ID сообщения не найдено");
-			const message = await Main.getDiscordMessageByID(interaction.channelId, message_id);
-			if (message === Main.ErrorMessages.CHANNEL_WRONG_TYPE || message === Main.ErrorMessages.CHANNEL_NOT_FOUND || message === Main.ErrorMessages.MESSAGE_NOT_FOUND) throw new Error(message);
-			const value = values[1]; if (!value || value.length < 1) throw new Error("Название не найдено");
-			title = value;
-
-			await (await Main.getThread(message)).send(Main.getDiscordMessagePrefix(`:speech_left: Название стрима: **${title}**`));
-			await message.edit(Messages.streamStart(user, {
-				title,
-				games,
-				started_at
-			}, {
-				viewer_count,
-				thumbnail_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPg3Z19paW_g0iFfa5k0iJjFR4HKyAVkl6A&s"
-			}));
+		else if (type === "getchannelslength") {
 			await interaction.editReply({embeds: [new EmbedBuilder()
 				.setTitle(`:white_check_mark: Успешно!`)
+				.setDescription(`\`\`\`\n${Object.keys(Main.data.global.channels).length}\n\`\`\``)
 				.setColor("#77b255")
 				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
 			]});
 		}
-		else if (type === "testmessagechangegame") {
-			const message_id = values[0]; if (!message_id || message_id.length < 1) throw new Error("ID сообщения не найдено");
-			const message = await Main.getDiscordMessageByID(interaction.channelId, message_id);
-			if (message === Main.ErrorMessages.CHANNEL_WRONG_TYPE || message === Main.ErrorMessages.CHANNEL_NOT_FOUND || message === Main.ErrorMessages.MESSAGE_NOT_FOUND) throw new Error(message);
-			const value = values[1]; if (!value || value.length < 1) throw new Error("Игра не найдена");
-			games.push(value);
-
-			await (await Main.getThread(message)).send(Main.getDiscordMessagePrefix(`:video_game: Текущая игра: **${title}**`));
-			await message.edit(Messages.streamStart(user, {
-				title,
-				games,
-				started_at
-			}, {
-				viewer_count,
-				thumbnail_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPg3Z19paW_g0iFfa5k0iJjFR4HKyAVkl6A&s"
-			}));
+		else if (type === "gettoken") {
 			await interaction.editReply({embeds: [new EmbedBuilder()
 				.setTitle(`:white_check_mark: Успешно!`)
+				.setFields(
+					{
+						name: "Токен доступа",
+						value: `\`${Main.data.global.access_token}\``
+					},
+					{
+						name: "Refresh-токен",
+						value: `\`${Main.data.global.refresh_token}\``
+					}
+				)
 				.setColor("#77b255")
 				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
 			]});
-		}
-		else if (type === "testmessageendstream") {
-			const message_id = values[0]; if (!message_id || message_id.length < 1) throw new Error("ID сообщения не найдено");
-			const message = await Main.getDiscordMessageByID(interaction.channelId, message_id);
-			if (message === Main.ErrorMessages.CHANNEL_WRONG_TYPE || message === Main.ErrorMessages.CHANNEL_NOT_FOUND || message === Main.ErrorMessages.MESSAGE_NOT_FOUND) throw new Error(message);
-
-			await message.edit(Messages.streamEnd(user, {
-				title,
-				games,
-				started_at,
-				ended_at: new Date().toISOString()
-			}));
-			await interaction.editReply({embeds: [new EmbedBuilder()
-				.setTitle(`:white_check_mark: Успешно!`)
-				.setColor("#77b255")
-				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
-			]});
-			await (await Main.getThread(message)).send(Main.getDiscordMessagePrefix(":red_circle: Стрим окончен"));
-		}
-		else if (type === "testmessageendstreamvod") {
-			const message_id = values[0]; if (!message_id || message_id.length < 1) throw new Error("ID сообщения не найдено");
-			const message = await Main.getDiscordMessageByID(interaction.channelId, message_id);
-			if (message === Main.ErrorMessages.CHANNEL_WRONG_TYPE || message === Main.ErrorMessages.CHANNEL_NOT_FOUND || message === Main.ErrorMessages.MESSAGE_NOT_FOUND) throw new Error(message);
-
-			await message.edit(Messages.streamEndWithVOD(user, {
-				title,
-				games,
-				started_at,
-				ended_at: new Date().toISOString()
-			}, {
-				url: "https://www.youtube.com/watch?v=eTplxWaAD8o"
-			}));
-			await interaction.editReply({embeds: [new EmbedBuilder()
-				.setTitle(`:white_check_mark: Успешно!`)
-				.setColor("#77b255")
-				.setFooter({text: `Время обработки: ${humanizeDuration(Date.now() - start)}`})
-			]});
-			await (await Main.getThread(message)).send(Main.getDiscordMessagePrefix(":vhs: Получена запись стрима"));
 		}
 		else
 			throw new Error("Неизвестная команда");
